372609fbfd9e3828a57d66ded109fdca3c226cc6
Michael Florez <maicol_1702@hotmail.com>
2024-05-31 09:09:56 -0500

    Agregando solucion punto 2.c y punto 3.b del worshop y paso a paso de ejecuacion de comando basicos de GIT

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ console.log(resultado);
 // prompt();
 // confirm();
 // document.write();
+
+//-----------------------------------------------------------------------
+//Solucion item c punto 2 workshop
+
+// Declaracion de variables
+
+const a = 4;
+const b = 7;
+const c = 2 * b;
+const x = a;
+const y = 3 * x;
+const u = 6;
+const w = 10;
+const z = 2 * w;
+
+//Proceso
+
+resultado = (x + y) / ( u + w / a)
+
+//Mostrar resultado 
+console.log("2.C ",resultado);
+
+//Punto 3 item b
+const pTrue = true;
+const qTrue = true;
+const pFalse = false;
+const qFalse = false;
+
+//------- Proceso; cuando p y q son verdaderas
+resultado = !pTrue && !qTrue;
+
+//3. Mostrar resultados
+console.log("3.B. NOTp AND NOTq", resultado);
